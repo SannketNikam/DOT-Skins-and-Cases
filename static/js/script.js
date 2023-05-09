f055f88dbcf7a125490678704f331093519b0a80
Sanket Nikam <sanketn734@gmail.com>
2023-05-09 19:03:18 +0530

    Added Javascript

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+window.onload = function user_name(user) {
+    var username
+}
+// {% for login in allLogin %}
+//                   {% if loop.last %}
+//                     &nbsp;&nbsp;{{ login.fullname }}
+//                   {% endif %}
+//                 {% endfor %}
